92ffa4997d72a97125c764cc364e068de3610b65
pkp24 <37089940+pkp24@users.noreply.github.com>
2025-07-14 11:09:29 -0400

    Fix carry capacity adjustment logic

--- Source/PickUpAndHaul/WorkGiver_HaulToInventory.cs
+++ Source/PickUpAndHaul/WorkGiver_HaulToInventory.cs
@@ -630,25 +630,31 @@ public class WorkGiver_HaulToInventory : WorkGiver_HaulGeneral
 				break;
 			}
 
-			if (carryCapacity <= 0)
-			{
-				var originalCount = job.countQueue.Pop();
-				var adjustedCount = originalCount + carryCapacity;
-				
-				if (adjustedCount <= 0)
-				{
-					// Remove the item entirely if the adjusted count is 0 or negative
-					job.targetQueueA.RemoveAt(job.targetQueueA.Count - 1);
-					job.targetQueueB.RemoveAt(job.targetQueueB.Count - 1);
-					Log.Message($"[PickUpAndHaul] DEBUG: Removed last item from job - adjusted count would be {adjustedCount} (original: {originalCount}, carryCapacity: {carryCapacity})");
-				}
-				else
-				{
-					job.countQueue.Add(adjustedCount);
-					Log.Message($"[PickUpAndHaul] DEBUG: Adjusted count from {originalCount} to {adjustedCount} (carryCapacity: {carryCapacity})");
-				}
-				break;
-			}
+                        if (carryCapacity <= 0)
+                        {
+                                var originalCount = job.countQueue.Pop();
+                                var adjustedCount = originalCount + carryCapacity;
+
+                                if (adjustedCount <= 0)
+                                {
+                                        // Remove the item entirely if the adjusted count is 0 or negative
+                                        job.targetQueueA.RemoveAt(job.targetQueueA.Count - 1);
+
+                                        // Only remove from targetQueueB when it was created for this item
+                                        if (job.targetQueueB.Count == job.targetQueueA.Count + 1)
+                                        {
+                                                job.targetQueueB.RemoveAt(job.targetQueueB.Count - 1);
+                                        }
+
+                                        Log.Message($"[PickUpAndHaul] DEBUG: Removed last item from job - adjusted count would be {adjustedCount} (original: {originalCount}, carryCapacity: {carryCapacity})");
+                                }
+                                else
+                                {
+                                        job.countQueue.Add(adjustedCount);
+                                        Log.Message($"[PickUpAndHaul] DEBUG: Adjusted count from {originalCount} to {adjustedCount} (carryCapacity: {carryCapacity})");
+                                }
+                                break;
+                        }
 		}
 
 		Log.Message($"[PickUpAndHaul] DEBUG: Final job state before return:");
