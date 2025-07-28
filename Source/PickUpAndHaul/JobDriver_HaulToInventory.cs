2d4c7ca5d512d9305aea139f338ab4cd2b064b24
pkp24 <37089940+pkp24@users.noreply.github.com>
2025-07-27 20:15:41 -0400

    Fix Null Job Access in Hauling AI

--- Source/PickUpAndHaul/JobDriver_HaulToInventory.cs
+++ Source/PickUpAndHaul/JobDriver_HaulToInventory.cs
@@ -74,7 +74,7 @@ public class JobDriver_HaulToInventory : JobDriver
 				//This will technically release the reservations in the queue, but what can you do
 				if (thing.Spawned)
 				{
-					var haul = HaulAIUtility.HaulToStorageJob(actor, thing, actor.CurJob.playerForced);
+					var haul = HaulAIUtility.HaulToStorageJob(actor, thing, job.playerForced);
 					if (haul?.TryMakePreToilReservations(actor, false) ?? false)
 					{
 						actor.jobs.jobQueue.EnqueueFirst(haul, JobTag.Misc);
@@ -164,7 +164,7 @@ public class JobDriver_HaulToInventory : JobDriver
 
 			if (!(MassUtility.EncumbrancePercent(actor) <= 0.9f && !ceOverweight))
 			{
-				var haul = HaulAIUtility.HaulToStorageJob(actor, nextThing, curJob.playerForced);
+				var haul = HaulAIUtility.HaulToStorageJob(actor, nextThing, job.playerForced);
 				if (haul?.TryMakePreToilReservations(actor, false) ?? false)
 				{
 					//note that HaulToStorageJob etc doesn't do opportunistic duplicate hauling for items in valid storage. REEEE
